2adc9621224ec7e2abe32344c5db6c0b74bd6585
emmanuel <rai.rusianaemmanuel@gmail.com>
2026-03-09 16:00:11 +0800

    hello world

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = function (api) {
       'nativewind/babel',
     ],
     plugins: [
-      'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-worklets/plugin',
     ],
   };
 };
