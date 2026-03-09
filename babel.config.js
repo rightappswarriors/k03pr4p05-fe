6fd4cc08f5a3f66816206cca0bf206fafc300211
emmanuel <rai.rusianaemmanuel@gmail.com>
2026-03-09 13:46:22 +0800

    removed worklets

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = function (api) {
     ],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin', // ← replace with this
+      'react-native-reanimated/plugin',
     ],
   };
 };
