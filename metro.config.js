a62d0b9a28b7fc767b3b76dc8a26f8f661fd140a
Rusiana Emmanuel <rai.rusianaemmanuel@gmail.com>
2026-05-30 18:20:49 +0800

    fixed native wind removing

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-config.resolver.unstable_enablePackageExports = false;
 
-// Exclude react-native-maps for web platform
+const config = getDefaultConfig(__dirname)
+
+config.resolver.unstable_enablePackageExports = false;
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (platform === 'web') {
     if (moduleName.includes('react-native-maps') || moduleName === '../Utilities/Platform' || moduleName === './PlatformColorValueTypes' || moduleName.includes('ReactNativePrivateInterface') || moduleName.includes('ReactNativeViewConfigRegistry') || moduleName.includes('createReactNativeComponentClass') || moduleName.includes('requireNativeComponent') || moduleName.includes('codegenNativeComponent') || moduleName.includes('react-native-external-display')) {
@@ -15,4 +12,4 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
   return context.resolveRequest ? context.resolveRequest(context, moduleName, platform) : null;
 };
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = config;
